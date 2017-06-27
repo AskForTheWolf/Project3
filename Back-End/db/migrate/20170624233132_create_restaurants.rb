efd1d6e125401f111947af4227ef910b5e6b448b
Robert Lowe <RobLowe1992@Yahoo.com>
2017-06-27 10:33:08 -0400

    Index Style changes

--- Back-End/db/migrate/20170624233132_create_restaurants.rb
+++ Back-End/db/migrate/20170624233132_create_restaurants.rb
@@ -2,9 +2,10 @@ class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
     create_table :restaurants do |t|
       t.string :address
-      t.string :open
-      t.string :close
+      t.integer :open
+      t.integer :close
       t.string :name
+      t.decimal :rating
       t.string :menu, array: true
       t.boolean :featured
       t.timestamps
