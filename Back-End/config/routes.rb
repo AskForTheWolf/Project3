c39bbd5f51e46f73c5f4e088d63d79480de2d50f
Bao Cong Pham <BaoPham92@yahoo.com>
2017-06-27 18:37:44 -0400

    all controllers done

--- Back-End/config/routes.rb
+++ Back-End/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-	resources :restaurants do
+	resources :restaurants, only: [:index, :show] do
 		resources :order
 	end
+	resources :blogs, only: [:index, :show]
+
 
 	  root to: redirect('/restaurants')
 end
--- Front-End/js/app.js
+++ Front-End/js/app.js
@@ -1,20 +1,58 @@
 angular
 .module('delivery-clone', ['ui.router', 'ngResource'])
 .config(['$stateProvider', RouterFunction])
-.controller('DCLandingController',['RestaurantFactory',function(RestaurantFactory){
+.controller('RootController',['RestaurantFactory',function(RestaurantFactory){
     // this.featured = RestaurantFactory.get({featured: true})
 }])
-.controller('DCIndexController',['RestaurantFactory',function(RestaurantFactory){
+
+// Below is Controllers for 'Restaurant'
+
+.controller('RestaurantIndexController',['RestaurantFactory',function(RestaurantFactory){
     this.restaurants = RestaurantFactory.query()
 }])
-.controller('DCShowController',['$stateParams','RestaurantFactory',function($stateParams ,RestaurantFactory){
-    this.restaurant = RestaurantFactory.get({id: $stateParams.id})
+.controller('RestaurantShowController',['$stateParams','RestaurantFactory',function($stateParams ,RestaurantFactory){
+    this.restaurant = RestaurantFactory.get({id: $stateParams.id })
     console.log(this.restaurant)
 }])
+
+// Below is Controllers for 'Blog'
+
+.controller('BlogIndexController', ['RestaurantFactory', function($RestaurantFactory){
+    this.blog = RestaurantFactory.query({id: $stateParams.id })
+    console.log(this.blog)
+}])
+.controller('BlogShowController', ['$stateParams','RestaurantFactory', function($stateParams, RestaurantFactory){
+    this.blog = RestaurantFactory.get({id: $stateParams.id })
+    console.log(this.blog)
+}])
+
+// Below is Controllers for 'Order'
+
 .controller('OrderIndexController', ['RestaurantFactory', function(RestaurantFactory){
     this.orders = RestaurantFactory.query()
 }])
-.controller('OrderShowController', ['RestaurantFactory', function($stateParams, RestaurantFactory){
+.controller('OrderCreateController', ['$stateParams', 'RestaurantFactory', function($stateParams, RestaurantFactory){
+    this.order = RestaurantFactory.create({
+        restaurant_id: $stateParams.restaurant_id,
+        id: $stateParams.id
+    })
+    console.log(this.order)
+}])
+.controller('OrderNewController', ['$stateParams', 'RestaurantFactory', function($stateParams, RestaurantFactory){
+    this.order = RestaurantFactory.({
+        restaurant_id: $stateParams.restaurant_id,
+        id: $stateParams.id
+    })
+    console.log(this.order)
+}])
+.controller('OrderEditController', ['$stateParams', 'RestaurantFactory', function($stateParams, RestaurantFactory){
+    this.order = RestaurantFactory.edit({
+        restaurant_id: $stateParams.restaurant_id,
+        id: $stateParams.id
+    })
+    console.log(this.order)
+}])
+.controller('OrderShowController', ['$stateParams','RestaurantFactory', function($stateParams, RestaurantFactory){
     this.order = RestaurantFactory.get({
         restaurant_id: $stateParams.restaurant_id,
         id: $stateParams.id
@@ -22,6 +60,21 @@ angular
     this.itemDets = [];
     console.log(this.order)
 }])
+.controller('OrderUpdateController', ['$stateParams', 'RestaurantFactory', function($stateParams, RestaurantFactory){
+    this.order = RestaurantFactory.update({
+        restaurant_id: $stateParams.restaurant_id,
+        id: $stateParams.id
+    })
+    console.log(this.order)
+}])
+.controller('OrderDestroyController', ['$stateParams', 'RestaurantFactory', function($stateParams, RestaurantFactory){
+    this.order = RestaurantFactory.destroy({
+        restaurant_id: $stateParams.restaurant_id,
+        id: $stateParams.id
+    })
+    console.log(this.order)
+}])
+.controller('')
 .factory('RestaurantFactory', ['$resource', function($resource){
         return $resource('http://localhost:3000/restaurants/:id.json',{},{'query': {method: 'GET', isArray: true}})
 }])
@@ -33,16 +86,16 @@ function RouterFunction($stateProvider){
         controller: 'DCLandingController',
         controllerAs: 'vm'
     })
-    .state('dcIndex', {
+    .state('RestaurantIndex', {
         url: '/restaurants',
         templateUrl: 'js/ng-views/restaurant-index.html',
-        controller: 'DCIndexController',
+        controller: 'RestaurantIndexController',
         controllerAs: 'vm'
     })
-    .state('dcShow', {
+    .state('RestaurantShow', {
         url: '/restaurants/:id',
         templateUrl: 'js/ng-views/restaurant-show.html',
-        controller: 'DCShowController',
+        controller: 'RestaurantShowController',
         controllerAs: 'vm'
     })
     .state('OrderIndex', {
